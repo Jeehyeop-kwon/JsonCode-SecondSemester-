a5ba0579c83a09f92b4bbb1de05e3660c5a50ddc
Jeehyeop-kwon <jeehyeop@naver.com>
2019-03-29 13:03:18 -0400

    Add files via upload

---
+++ JsonHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace JsonSpring
+{
+    public class JsonHelper
+    {
+        private string jsonFile = @"{'email':'jeehyeop@naver.com', 'firstname':'jeehyuep', 'lastname':'kwon'}";
+
+        public Student JsonParser()
+        {
+            Student s = JsonConvert.DeserializeObject<Student>(jsonFile);
+            return s;        
+        }
+
+        public void ReadJsonFile(string fileName) 
+        {
+            using (FileStream fileStream = new FileStream(fileName, FileMode.Open))
+            using (StreamReader streamReader = new StreamReader(fileStream))
+            {
+                jsonFile = streamReader.ReadToEnd();
+            }
+        }
+
+
+    }
+
+    public class Student
+    {
+        public string email;
+        public string firstName;
+        public string lastName;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace JsonSpring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JsonHelper jsonHelper = new JsonHelper();
+            jsonHelper.ReadJsonFile(@"/Users/jeehyeopkwon/Desktop/test.json");
+
+            Student s = jsonHelper.JsonParser();
+            Console.WriteLine(s.email);
+            Console.WriteLine(s.firstName);
+            Console.WriteLine(s.lastName);
+        }
+    }
+}
